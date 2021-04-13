fa80f3ad0ac26b79ad4761d62a88df28a272a643
muzyk0 <ru9art@gmail.com>
2021-04-13 21:10:47 +0300

    setInterval tests

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -4,7 +4,7 @@ export default {
     title: 'UseEffect demo'
 }
 
-export const UseEffect = () => {
+export const SimpleExample = () => {
     const [fake, setFake] = useState(1)
     const [counter, setCounter] = useState(1)
 
@@ -28,5 +28,39 @@ export const UseEffect = () => {
         {counter}
         <button onClick={() => setFake(state => state + 1)}>fake+</button>
         <button onClick={() => setCounter(state => state + 1)}>counter+</button>
+    </>
+}
+
+export const SetTimeoutExample = () => {
+    const [counter, setCounter] = useState(1)
+
+    console.log('timeout example render')
+
+    useEffect(() => {
+        setInterval(() => {
+            setCounter(state => state + 1)
+        }, 1000)
+    }, [])
+
+    return <>
+        counter: {counter}
+    </>
+}
+
+export const DateExample     = () => {
+    const [date, setDate] = useState(() => {
+        return new Date()
+    })
+    console.log(date)
+    console.log('timeout example render')
+
+    useEffect(() => {
+        setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+    }, [])
+
+    return <>
+        date: {`${date.getFullYear()}.${date.getMinutes()}.${date.getSeconds()}`}
     </>
 }
